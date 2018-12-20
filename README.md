65656cad77a83f7b8685471ebdfc5f243e7377df
Pedre Viljoen <pedreviljoen@Pedres-MacBook-Pro.local>
2018-12-20 09:32:22 +0200

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AdvancedNodeStarter
 Starting project for a course on Advanced Node @ Udemy
+
+# Redis
+
+Run the following command to start redis:
+
+```sh
+brew services start redis
+```
+
+Restart redis:
+
+```sh
+brew services restart redis
+```
---
+++ services/cache.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const exec = mongoose.Query.prototype.exec
+const redis = require('redis')
+const util = require('util')
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl)
+client.get = util.promisify(client.get)
+
+mongoose.Query.prototype.cache = function () {
+    this._cache = true
+    return this
+}
+
+mongoose.Query.prototype.exec = async function () {
+    console.log(`[QUERY]: ${this.getQuery()}`)
+    console.log(`[COL]: ${this.mongooseClient.name}`)
+
+    if (!this._cache) {
+        return exec.apply(this, arguments)
+    }
+
+    const key = JSON.stringify(
+        Object.assign({}, this.getQuery(), {
+            collection: this.mongooseClient.name
+        })
+    )
+
+    const cacheValue = await client.get(key)
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue)
+
+        return Array.isArray(doc) 
+            ? (doc.map(d => new this.model(d))) 
+            : new this.model(doc)
+    }
+
+    const result = await exec.apply(this, arguments)
+    client.set(key, JSON.stringify(result), 'EX', 10)
+    return result
+}
